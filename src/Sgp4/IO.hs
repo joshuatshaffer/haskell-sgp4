817c321b4d375e0c5ee81add73104663c501fec1
joshuatshaffer <joshuatshaffer@gmail.com>
2017-05-10 16:33:28 -0500

    Fixed small errors in IO.

--- src/Sgp4/IO.hs
+++ src/Sgp4/IO.hs
@@ -10,12 +10,15 @@ sgp4OrbitFromTLE (TLE _ _ _ epochYear epochDay n' n'' bStar _ _ i _Ω e ω m0 n 
              (i * pi / 180.0)
              (_Ω * pi / 180.0)
              (ω * pi / 180.0)
-             (n * 2 * pi / 1440.0)
-             (n' * 2 * pi / 1440.0**2)
-             (n'' * 2 * pi / 1440.0**3)
+             (n / xpdotp)
+             (n' / (xpdotp * 1440.0))
+             (n'' / (xpdotp * 1440.0 * 1440))
              (m0 * pi / 180.0)
              bStar
              (jday epochYear (days2mdhms epochYear epochDay) - 2433281.5)
+  where
+    xpdotp :: Double
+    xpdotp = 1440.0 / (2.0 * pi)
 
 -- "1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927"
 -- "2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537"
@@ -32,7 +35,7 @@ charClassAliases '.' = "\\."
 charClassAliases x = [x]
 
 tlePatternLn1, tlePatternLn2 :: String
-tlePatternLn1 = concatMap charClassAliases "1 (N{5})(C) (N{5}A{3}) (N{2})(N{3}.N{8}) (+.N{8}) (+N{5}-N) (+N{5}-N) 0 (N{4})(N)"
+tlePatternLn1 = concatMap charClassAliases "1 (N{5})(C) (N{5}A{3}) (N{2})(N{3}.N{8}) (+.N{8}) (+N{5})(-N) (+N{5})(-N) 0 (N{4})(N)"
 tlePatternLn2 = concatMap charClassAliases "2 (N{5}) (N{3}.N{4}) (N{3}.N{4}) (N{7}) (N{3}.N{4}) (N{3}.N{4}) (N{2}.N{8})(N{5})(N)"
 
 parceTLE :: String -> String -> TLE
@@ -42,10 +45,10 @@ parceTLE ln1 ln2 = TLE (read $ head l1m)
                        ((\x -> if x < 57 then x+2000 else x + 1900) . read $ l1m !! 3)
                        (read $ l1m !! 4)
                        (read . (\(x:xs)->x:'0':xs) $ l1m !! 5)
-                       (read . pudunk $ l1m !! 6)
-                       (read . pudunk $ l1m !! 7)
-                       (read $ l1m !! 8)
-                       (read $ l1m !! 9)
+                       ((read . pudunk $ l1m !! 6) * 10.0 ** read (l1m !! 7))
+                       ((read . pudunk $ l1m !! 8) * 10.0 ** read (l1m !! 9))
+                       (read $ l1m !! 10)
+                       (read $ l1m !! 11)
                        (read $ l2m !! 1)
                        (read $ l2m !! 2)
                        (read . ("0."++) $ l2m !! 3)
@@ -59,4 +62,4 @@ parceTLE ln1 ln2 = TLE (read $ head l1m)
     (_,_,_,l2m) = ln2 =~ tlePatternLn2 :: (String,String,String,[String])
 
     pudunk :: String -> String
-    pudunk s = head s : "0." ++ drop 1 (take 5 s) ++ "e" ++ drop 6 s
+    pudunk s = head s : "0." ++ drop 1 s
